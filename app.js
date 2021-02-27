5e294e2a1479c69648df70e5e20effc752c21048
bgdnvk <bnovykov@gmail.com>
2021-02-27 04:40:21 +0100

    start n dev and endpoint

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use(middleware.requestLogger)
-app.use('/api/blogs', blogsRouter)
+app.use('/', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
